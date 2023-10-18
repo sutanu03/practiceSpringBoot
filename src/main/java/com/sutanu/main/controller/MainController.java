1d92b29c1ad6b9b3454839a41d86bc8bdce80526
Sutanu Ghosal <csesutanu@gmail.com>
2023-10-18 14:07:01 +0530

    crud application modified

--- src/main/java/com/sutanu/main/controller/MainController.java
+++ src/main/java/com/sutanu/main/controller/MainController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 //import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -23,12 +24,12 @@ public class MainController {
 	@Autowired
 	ProgrammerRepo pr;
 	
-	@ModelAttribute
-	public void welcome(Model m)
-	{
-		m.addAttribute("m","Welcome Sutanu");
-	}
-	
+//	@ModelAttribute
+//	public void welcome(Model m)
+//	{
+//		m.addAttribute("m","Welcome Sutanu");
+//	}
+//	
 	
 	@GetMapping("/indexPage")
 	public String homepage()
@@ -56,10 +57,20 @@ public class MainController {
 	
 */	
 
+	// creating new programmer
 	@PostMapping("/addedProgrammer")
 	public String addedProgrammer(@ModelAttribute Programmer programmer)
 	{
 		pr.save(programmer);
+		// return "ProgrammerInfo.html";
+		return"redirect:/indexPage";
+	}
+	
+	// read programmer details by ID
+	@PostMapping("/readData")
+	public String readProgrammerDetails(@RequestParam int pID, Model m) {
+		Programmer p = pr.getOne(pID);
+		m.addAttribute("programmer", p);
 		return "ProgrammerInfo.html";
 	}
 	
